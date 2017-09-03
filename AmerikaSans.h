748dcdb83c84d8eda5905ea4526677fca9136ffe
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 22:18:12 +0100

    Initial import

---
+++ AmerikaSans.h
@@ -0,0 +1,39 @@
+#ifndef _AMERIKASANS_H
+#define _AMERIKASANS_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t AmerikaSans10[];
+    extern const uint8_t AmerikaSans11[];
+    extern const uint8_t AmerikaSans12[];
+    extern const uint8_t AmerikaSans13[];
+    extern const uint8_t AmerikaSans14[];
+    extern const uint8_t AmerikaSans15[];
+    extern const uint8_t AmerikaSans16[];
+    extern const uint8_t AmerikaSans17[];
+    extern const uint8_t AmerikaSans18[];
+    extern const uint8_t AmerikaSans19[];
+    extern const uint8_t AmerikaSans20[];
+    extern const uint8_t AmerikaSans21[];
+    extern const uint8_t AmerikaSans22[];
+    extern const uint8_t AmerikaSans23[];
+    extern const uint8_t AmerikaSans24[];
+    extern const uint8_t AmerikaSans25[];
+    extern const uint8_t AmerikaSans26[];
+    extern const uint8_t AmerikaSans27[];
+    extern const uint8_t AmerikaSans28[];
+    extern const uint8_t AmerikaSans29[];
+    extern const uint8_t AmerikaSans30[];
+    extern const uint8_t AmerikaSans31[];
+    extern const uint8_t AmerikaSans32[];
+    extern const uint8_t AmerikaSans33[];
+    extern const uint8_t AmerikaSans34[];
+    extern const uint8_t AmerikaSans35[];
+    extern const uint8_t AmerikaSans36[];
+    extern const uint8_t AmerikaSans37[];
+    extern const uint8_t AmerikaSans38[];
+    extern const uint8_t AmerikaSans39[];
+    extern const uint8_t AmerikaSans40[];
+};
+#endif
